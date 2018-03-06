92f6ebab2e9c763b2306585a34eb324c1d1f01f2
Ty Richadson <tyLrichardson63@gmail.com>
2018-03-06 16:05:58 -0600

    6-March Group Project

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+/* We didn't realize there was a Repo with this info. We are pasting out work in below.
 const atticus = { name: 'Atticus', employeeNumber: '2405', annualSalary: '47000', reviewRating: 3 };
 const jem = { name: 'Jem', employeeNumber: '62347', annualSalary: '63500', reviewRating: 4 };
 const scout = { name: 'Scout', employeeNumber: '6243', annualSalary: '74750', reviewRating: 5 };
@@ -9,3 +10,34 @@ const employees = [ atticus, jem, scout, robert, mayella ];
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 console.log(employees);
+*/
+
+//console.log('js');
+
+
+//extractFromArray();
+
+
+console.log('js');
+//create object
+let testEmployee = {name: 'Ty', employeeNumber: 4004, annualSalary: 40000.00, reviewRating: 4};
+class Employee {
+  constructor(nameIn, employeeNumberIn, annualSalaryIn, reviewRatingIn) {
+    this.name = nameIn;
+    this.employeeNumber = employeeNumberIn;
+    this.annualSalary = annualSalaryIn;
+    this.reviewRating = reviewRatingIn;
+  }
+}
+
+function inputEmployee(employeeInputted) {
+  let updatedEmployee = new Employee(employeeInputted.name, employeeInputted.employeeNumber,
+    employeeInputted.annualSalary, employeeInputted.reviewRating);
+  //return updatedEmployee;
+  console.log(updatedEmployee);
+}
+inputEmployee(testEmployee);
+
+function bonusCalc(){
+
+}
